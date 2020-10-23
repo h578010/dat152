20b82e85db632963eabc8325f5e7518095e6d296
Synnøve Sørensen <578010@stud.hvl.no>
2020-10-23 22:19:06 +0200

    Changed to prepared statements in AppUserDAO

--- DAT152ApplicationOblig/src/no/hvl/dat152/obl4/database/AppUserDAO.java
+++ DAT152ApplicationOblig/src/no/hvl/dat152/obl4/database/AppUserDAO.java
@@ -8,14 +8,12 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.xml.bind.DatatypeConverter;
-
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class AppUserDAO {
 
-  public AppUser getAuthenticatedUser(String username, String password) {
+  public AppUser getAuthenticatedUser(String username, String password) throws NoSuchAlgorithmException {
 
     String hashedPassword = generatePassHash(password);
 
@@ -27,14 +25,14 @@ public class AppUserDAO {
     AppUser user = null;
 
     Connection c = null;
-    Statement s = null;
+    PreparedStatement s = null;
     ResultSet r = null;
 
     try {        
       c = DatabaseHelper.getConnection();
       s = c.prepareStatement(sql);
-      ((PreparedStatement) s).setString(1, username);
-      ((PreparedStatement) s).setString(2, password);
+      s.setString(1, username);
+      s.setString(2, password);
       r = s.executeQuery(sql);
 
       if (r.next()) {
@@ -59,32 +57,33 @@ public class AppUserDAO {
 
   public boolean saveUser(AppUser user) {
 
-    String sql = "INSERT INTO SecOblig.AppUser VALUES (" 
-        + "'" + user.getUsername()  + "', "
-        + "'" + user.getPasshash()  + "', "
-        + "'" + user.getFirstname() + "', "
-        + "'" + user.getLastname()  + "', "
-        + "'" + user.getMobilephone()  + "', "
-        + "'" + user.getRole() + "')";
+    String sql = "INSERT INTO SecOblig.AppUser VALUES (?, ?, ?, ?, ?, ?)";
 
     Connection c = null;
-    Statement s = null;
+    PreparedStatement s = null;
     ResultSet r = null;
 
-    try {        
-      c = DatabaseHelper.getConnection();
-      s = c.createStatement();       
-      int row = s.executeUpdate(sql);
-      if(row >= 0)
-    	  return true;
-    } catch (Exception e) {
-    	System.out.println(e);
-    	return false;
-    } finally {
-      DatabaseHelper.closeConnection(r, s, c);
-    }
-    
-    return false;
+    try {
+		c = DatabaseHelper.getConnection();
+		s = c.prepareStatement(sql);
+		s.setString(1, user.getUsername());
+		s.setString(2, user.getPasshash());
+		s.setString(3, user.getFirstname());
+		s.setString(4, user.getLastname());
+		s.setString(5, user.getMobilephone());
+		s.setString(6, user.getRole());
+		int row = s.executeUpdate();
+		if (row >= 0) {
+			return true;
+		}
+	} catch (Exception e) {
+		System.out.println(e);
+		return false;
+	} finally {
+		DatabaseHelper.closeConnection(r, s, c);
+	}
+
+	return false;
   }
   
   public List<String> getUsernames() {
@@ -115,61 +114,62 @@ public class AppUserDAO {
 	  return usernames;
   }
   
-  public boolean updateUserPassword(String username, String passwordnew) {
+  public boolean updateUserPassword(String username, String passwordnew) throws NoSuchAlgorithmException {
 	  
 	  String hashedPassword = generatePassHash(passwordnew);
 	  
-	    String sql = "UPDATE SecOblig.AppUser "
-	    		+ "SET passhash = '" + hashedPassword + "' "
-	    				+ "WHERE username = '" + username + "'";
+	    String sql = "UPDATE SecOblig.AppUser SET passhash =? WHERE username =?";
 	
 	    Connection c = null;
-	    Statement s = null;
+	    PreparedStatement s = null;
 	    ResultSet r = null;
 	
-	    try {        
-	      c = DatabaseHelper.getConnection();
-	      s = c.createStatement();       
-	      int row = s.executeUpdate(sql);
-	      System.out.println("Password update successful for "+username);
-	      if(row >= 0)
-	    	  return true;
-	      
-	    } catch (Exception e) {
-	      System.out.println(e);
-	      return false;
-	    } finally {
-	      DatabaseHelper.closeConnection(r, s, c);
-	    }
-	    
-	    return false;
+		try {
+			c = DatabaseHelper.getConnection();
+			s = c.prepareStatement(sql);
+			s.setString(1, hashedPassword);
+			s.setString(2, username);
+			int row = s.executeUpdate();
+			if (row >= 0) {
+				System.out.println("Password update successful for " + username);
+				return true;
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+			return false;
+		} finally {
+			DatabaseHelper.closeConnection(r, s, c);
+		}
+
+		return false;
   }
   
   public boolean updateUserRole(String username, String role) {
 
-	    String sql = "UPDATE SecOblig.AppUser "
-	    		+ "SET role = '" + role + "' "
-	    				+ "WHERE username = '" + username + "'";
+	    String sql = "UPDATE SecOblig.AppUser SET role =? WHERE username =?";
 	
 	    Connection c = null;
-	    Statement s = null;
+	    PreparedStatement s = null;
 	    ResultSet r = null;
 	
-	    try {        
-	      c = DatabaseHelper.getConnection();
-	      s = c.createStatement();       
-	      int row = s.executeUpdate(sql);
-	      System.out.println("Role update successful for "+username+" New role = "+role);
-	      if(row >= 0)
-	    	  return true;
-	      
-	    } catch (Exception e) {
-	      System.out.println(e);
-	      return false;
-	    } finally {
-	      DatabaseHelper.closeConnection(r, s, c);
-	    }
-	    return false;
+	    try {
+			c = DatabaseHelper.getConnection();
+			s = c.prepareStatement(sql);
+			s.setString(1, role);
+			s.setString(2, username);
+			int row = s.executeUpdate();
+			if (row >= 0) {
+				System.out.println("Role update successful for " + username + " New role = " + role);
+				return true;
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			return false;
+		} finally {
+			DatabaseHelper.closeConnection(r, s, c);
+		}
+		return false;
   }
   
   // Changed algorithm from MD5 to SHA-256
--- DAT152ApplicationOblig/src/no/hvl/dat152/obl4/util/Validator.java
+++ DAT152ApplicationOblig/src/no/hvl/dat152/obl4/util/Validator.java
@@ -5,4 +5,8 @@ public class Validator {
 	public static String validString(String parameter) {
 		return parameter != null ? parameter : "null";
 	}
+	
+	public static boolean validPassword(String parameter) {
+		return parameter.matches("^([0-9a-zA-ZæøåÆØÅ]{8,30})$");
+	}
 }
