f94e8f13e89ee96a7eea159d6c4d8b995d831a44
Synnøve Sørensen <578010@stud.hvl.no>
2020-10-26 13:27:30 +0100

    Changed query to prepared statement

--- DAT152ApplicationOblig/src/no/hvl/dat152/obl4/database/SearchItemDAO.java
+++ DAT152ApplicationOblig/src/no/hvl/dat152/obl4/database/SearchItemDAO.java
@@ -1,6 +1,7 @@
 package no.hvl.dat152.obl4.database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -16,12 +17,34 @@ public class SearchItemDAO {
   }
 
   public List<SearchItem> getSearchHistoryForUser(String username) {
-	    String sql = "SELECT * FROM SecOblig.History " 
-	        + "WHERE username = '" + username 
-	        + "' ORDER BY datetime DESC";
-	    //  LIMIT 50
-	    // Derby lacks LIMIT
-	    return getSearchItemList(sql,50);
+	    String sql = "SELECT * FROM SecOblig.History WHERE username = ? ORDER BY datetime DESC";
+	    List<SearchItem> result = new ArrayList<SearchItem>();
+	    
+	    Connection c = null;
+		Statement s = null;
+		ResultSet r = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			c = DatabaseHelper.getConnection();
+			pstmt = c.prepareStatement(sql);
+			pstmt.setString(1, username);
+			pstmt.setMaxRows(50);
+			r = pstmt.executeQuery();
+
+			while (r.next()) {
+				SearchItem item = new SearchItem(r.getTimestamp("datetime"), r.getString("username"),
+						r.getString("searchkey"));
+				result.add(item);
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			DatabaseHelper.closeConnection(r, s, c);
+		}
+
+		return result;
 	  }
   
   public List<SearchItem> getSearchHistoryForUser(String username, String sortkey) {
@@ -68,25 +91,27 @@ public class SearchItemDAO {
 
   public void saveSearch(SearchItem search) {
 
-    String sql = "INSERT INTO SecOblig.History VALUES (" 
-        + "'" + search.getDatetime()  + "', "
-        + "'" + search.getUsername()  + "', "
-        + "'" + search.getSearchkey() + "')";
+    String sql = "INSERT INTO SecOblig.History VALUES (?, ?, ?)";
 
     Connection c = null;
-    Statement s = null;
+    PreparedStatement s = null;
     ResultSet r = null;
 
-    try {        
-      c = DatabaseHelper.getConnection();
-      s = c.createStatement();       
-      s.executeUpdate(sql);
+    try {
 
-    } catch (Exception e) {
-      System.out.println(e);
-    } finally {
-      DatabaseHelper.closeConnection(r, s, c);
-    }
+		c = DatabaseHelper.getConnection();
+		s = c.prepareStatement(sql);
+		s.setString(1, search.getDatetime().toString());
+		s.setString(2, search.getUsername());
+		s.setString(3, search.getSearchkey());
+
+		s.execute();
+
+	} catch (Exception e) {
+		System.out.println(e);
+	} finally {
+		DatabaseHelper.closeConnection(r, s, c);
+	}
   }
 
 }
